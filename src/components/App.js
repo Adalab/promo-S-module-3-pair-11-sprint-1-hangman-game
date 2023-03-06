a9fdf2249c067e278c2cdbd6f8c956589a3fe943
manuelainclan <minclanfigueiras@gmail.com>
2023-03-06 11:30:07 +0100

    Add incrementar button

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import '../styles/App.scss';
 
 function App() {
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    setNumberOfErrors(numberOfErrors+1);
+  }
   return (
     <div className='page'>
       <header>
@@ -47,7 +53,7 @@ function App() {
             />
           </form>
         </section>
-        <section className='dummy error-5'>
+        <section className={`dummy error-${numberOfErrors}`}>
           <span className='error-13 eye'></span>
           <span className='error-12 eye'></span>
           <span className='error-11 line'></span>
@@ -62,6 +68,7 @@ function App() {
           <span className='error-2 line'></span>
           <span className='error-1 line'></span>
         </section>
+        <button onClick={handleClick}>Incrementar</button>
       </main>
     </div>
   );
